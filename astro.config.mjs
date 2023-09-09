c8736ab270780a6bf83d919235e6cb82e8e8477e
mauricio <jourdanmauricio@gmail.com>
2023-09-09 01:24:37 -0300

    Git hub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+	site: 'https://jourdanmauricio.github.io',
+	base: '/blackwing-astro',
 })
